69f772b2378952d4480398cc4c32d7346910216f
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-07 15:38:30 -0500

    Crear menu.js

---
+++ plugins/sistema/menu.js
@@ -0,0 +1,33 @@
+// plugins/sistema/menu.js
+import { formatDuration } from '../../lib/utils.js'
+import config from '../../config.js'
+
+const handler = async ({ bot, from, reply, isOwner, db }) => {
+  const uptime = formatDuration(Date.now() - db.stats.startedAt)
+  const prefix = config.prefix
+
+  const menu = `╭━━━━━━━━━━━━━━━━━━━╮
+┃   🤖 *${config.botName}*
+╰━━━━━━━━━━━━━━━━━━━╯
+
+📊 *Stats*
+• Uptime: ${uptime}
+• Usuarios: ${Object.keys(db.users).length}
+• Mensajes: ${db.stats.totalMessages}
+
+⚙️ *Sistema*
+› ${prefix}ping
+› ${prefix}menu
+› ${prefix}info
+${isOwner ? `\n👑 *Owner*\n› ${prefix}ban\n› ${prefix}unban\n› ${prefix}backup\n› ${prefix}limpiar` : ''}
+
+> Prefijo: \`${prefix}\``
+
+  await reply(menu)
+}
+
+handler.command = ['menu', 'help', 'start', 'h']
+handler.tags = ['sistema']
+handler.help = ['menu']
+
+export default handler
