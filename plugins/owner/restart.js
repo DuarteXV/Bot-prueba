74b8c96b085f18234a6ec59e2d1c49fbc5e4bc3f
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-07 17:26:23 -0500

    Actualizar restart.js

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -1,21 +1,11 @@
-/**
- * Plugin de Reinicio para el sistema de plugins dinámicos
- */
 const handler = async (ctx) => {
-  const { bot, from, msg, reply, saveDB } = ctx
-  
+  const { reply, saveDB } = ctx
   await reply('🔄 *Reiniciando el sistema...*')
-  
-  // Guardamos la base de datos antes de cerrar
   await saveDB()
-  
-  // Esperamos un momento para asegurar que el mensaje se envíe
-  setTimeout(() => {
-    process.exit(0)
-  }, 2000)
+  setTimeout(() => { process.exit(0) }, 2000)
 }
 
 handler.command = ['restart', 'reinicio']
-handler.owner = true // Tu handler ya valida esto automáticamente
+handler.owner = true
 
 export default handler
