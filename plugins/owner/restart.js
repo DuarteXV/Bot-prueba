762a4ddcb94a634dfdfb4b6504b1999cdf93ee0e
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-07 17:31:34 -0500

    Actualizar restart.js

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -1,11 +1,18 @@
-const handler = async (ctx) => {
-  const { reply, saveDB } = ctx
+const handler = async ({ bot, from, reply, saveDB }) => {
   await reply('🔄 *Reiniciando el sistema...*')
+  
+  // Guardamos la base de datos
   await saveDB()
-  setTimeout(() => { process.exit(0) }, 2000)
+  
+  // Esperamos 2 segundos y cerramos el proceso
+  setTimeout(() => {
+    process.exit(0)
+  }, 2000)
 }
 
 handler.command = ['restart', 'reinicio']
+handler.tags = ['owner']
+handler.help = ['restart - Reinicia el bot']
 handler.owner = true
 
 export default handler
