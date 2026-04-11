d98750c23f311bb9edc8592d56bf7a2392fe968b
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-11 12:21:05 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ export default {
   channelJid: '120363420979328566@newsletter',
   channelName: '⏤͟͞ू⃪𝐁𝕃𝐔𝔼 𝐋𝕆𝐂𝕂 𝐂𝕃𝐔𝔹 𑁯🩵ᰍ',
   channelInviteLink: 'https://whatsapp.com/channel/0029Vb73g1r1NCrTbefbFQ2T',
+  channelThumbUrl: 'https://raw.githubusercontent.com/DuarteXV/Yotsuba-MD-Premium/main/uploads/d528f18702dbed84.jpg',
 
   subBots: [],
   tmpDir: './tmp',
