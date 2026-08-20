69e9ac9a25f77b76e65729b503ff54f98430fe40
DuarteXV <danielalejandrobasado@gmail.com>
2026-08-20 18:58:36 -0500

    Actualizar play.js

--- plugins/dl/play.js
+++ plugins/dl/play.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import yts from "yt-search";
 
-const API_KEY = "D
-Duarte-1311-2026";
+const API_KEY = "Duarte-1311-2026";
 
 export default {
   name: ["play", "yta", "ytmp3", "playaudio"],
